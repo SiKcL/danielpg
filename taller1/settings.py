e2084230967227dcaff770acabe2b006ab41c3ec
SiKcL <111404034+SiKcL@users.noreply.github.com>
2024-07-24 10:39:36 -0400

    ready for render.com

--- taller1/settings.py
+++ taller1/settings.py
@@ -54,8 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 
 ROOT_URLCONF = 'taller1.urls'
 
